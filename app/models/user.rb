85b349cfb60ca2ddda5598170e3c2db0762f4bd3
hungz23 <hungtranbuddha@gmail.com>
2016-07-07 11:41:43 +0700

    Make a basic User model (including secure passwords)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ActiveRecord::Base
-	has_many :microposts
-	validates :name, presence: true
-	validates :email, presence: true
+	before_save {email.downcase!}
+	validates :name, presence: true, length: {maximum: 50}
+	#validates :email, presence: true, length: {maximum: 250}
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  	validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: {case_sensitive: false}
+    has_secure_password
+    validates :password, presence: true, length: { minimum: 6 }
 end
